d013d3717e2b538b4b2f1d8fc8ff4460dbf7597e
OVK <mobile.ovk@gmail.com>
2019-02-09 20:07:43 +0400

    first Signed-off-by: OVK <mobile.ovk@gmail.com>

---
+++ term_today.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  term_today.py
+#
+#=======================================================================
+import os
+import sys
+import math
+import time
+import logging
+import smtplib
+import sqlite3
+from datetime import datetime
+from datetime import timezone
+import PySimpleGUI as sg
+#1======================================================================
+
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
