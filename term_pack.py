0d1f64d48e0f4e08888c86491d54f7748efd8040
OVK <mobile.ovk@gmail.com>
2019-02-11 05:25:58 +0400

    Signed-off-by: OVK <mobile.ovk@gmail.com>

---
+++ term_pack.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  term_pack.py
+#
+#=======================================================================
+import os
+import sys
+import math
+import time
+import logging
+import smtplib
+import sqlite3
+from datetime import datetime, timedelta, timezone
+#from datetime import timezone
+import operator
+if sys.version_info[0] >= 3:
+    import PySimpleGUI as sg
+else:
+    import PySimpleGUI27 as sg
+#=======================================================================
+
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
