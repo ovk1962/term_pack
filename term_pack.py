838bfcb915f077bd53a4dbb5b3373309d4e65634
OVK <mobile.ovk@gmail.com>
2019-02-11 05:29:40 +0400

    Signed-off-by: OVK <mobile.ovk@gmail.com>

--- term_pack.py
+++ term_pack.py
@@ -11,19 +11,149 @@ import time
 import logging
 import smtplib
 import sqlite3
-from datetime import datetime, timedelta, timezone
-#from datetime import timezone
-import operator
+from datetime import datetime
+from datetime import timezone
 if sys.version_info[0] >= 3:
     import PySimpleGUI as sg
 else:
     import PySimpleGUI27 as sg
 #=======================================================================
+class Class_LOGGER():
+    def __init__(self, path_log):
+        #self.logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger('__main__')
+        self.logger.setLevel(logging.INFO)
+        # create a file handler
+        self.handler = logging.FileHandler(path_log)
+        self.handler.setLevel(logging.INFO)
+        # create a logging format
+        #self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        self.handler.setFormatter(self.formatter)
 
+        # add the handlers to the logger
+        self.logger.addHandler(self.handler)
+    #-------------------------------------------------------------------
+    def wr_log_info(self, msg):
+        self.logger.info(msg)
+    #-------------------------------------------------------------------
+    def wr_log_error(self, msg):
+        self.logger.error(msg)
+#=======================================================================
+class Class_ACCOUNT():
+    def __init__(self):
+        self.acc_date = ''
+        self.acc_balance = 0.0
+        self.acc_profit  = 0.0
+        self.acc_go      = 0.0
+        self.acc_depo    = 0.0
+#=======================================================================
+class Class_FUT():
+    def __init__(self):
+        self.sP_code = "-"
+        self.sRest = 0
+        self.sVar_margin = 0.0
+        self.sOpen_price = 0.0
+        self.sLast_price = 0.0
+        self.sAsk =  0.0
+        self.sBuy_qty = 0
+        self.sBid =  0.0
+        self.sSell_qty = 0
+        self.sFut_go = 0.0
+#=======================================================================
+class Class_SQLite():
+    def __init__(self, path_db):
+        self.path_db = path_db
+        self.table_db = []
+        self.conn = ''
+        self.cur = ''
+    #-------------------------------------------------------------------
+    def check_db(self):
+        '''  check FILE of DB SQLite    -----------------------------'''
+        #    return os.stat: if FILE is and size != 0
+        r_check_db = [0, '']
+        name_path_db = self.path_db
+        if not os.path.isfile(name_path_db):
+            r_check_db = [1, 'can not find file']
+        else:
+            buf_st = os.stat(name_path_db)
+            if buf_st.st_size == 0:
+                r_check_db = [1, buf_st]
+            else:
+                r_check_db = [0, buf_st]
+        return r_check_db
+    #-------------------------------------------------------------------
+    def reset_table_db(self, name_tbl):
+        ''' reset data in table DB  ---------------------------------'''
+        r_reset_tbl = [0, '']
+        try:
+            self.conn = sqlite3.connect(self.path_db)
+            self.cur = self.conn.cursor()
+            self.cur.execute("DELETE FROM " + name_tbl)
+            self.conn.commit()
+            self.cur.close()
+            self.conn.close()
+            r_reset_tbl = [0, 'OK']
+        except Exception as ex:
+            r_reset_tbl = [1, str(ex)]
+        return r_reset_tbl
+    #-------------------------------------------------------------------
+    def rewrite_table(self, name_tbl, name_list, val = '(?, ?)'):
+        ''' rewrite data from table ARCHIV_PACK & PACK_TODAY & DATA ----'''
+        r_rewrt_tbl = [0, '']
+        try:
+            self.conn = sqlite3.connect(self.path_db)
+            self.cur = self.conn.cursor()
+            self.cur.execute("DELETE FROM " + name_tbl)
+            self.cur.executemany("INSERT INTO " + name_tbl + " VALUES" + val, name_list)
+            self.conn.commit()
+            self.cur.close()
+            self.conn.close()
+            r_rewrt_tbl = [0, 'OK']
+        except Exception as ex:
+            r_rewrt_tbl = [1, str(ex)]
+        return r_rewrt_tbl
+    #-------------------------------------------------------------------
+    def write_table_db(self, name_tbl, name_list):
+        ''' write data string into table DB  ------------------------'''
+        r_write_tbl = [0, '']
+        try:
+            self.conn = sqlite3.connect(self.path_db)
+            self.cur = self.conn.cursor()
+            self.cur.executemany("INSERT INTO " + name_tbl + " VALUES(?, ?)", name_list)
+            self.conn.commit()
+            self.cur.close()
+            self.conn.close()
+            r_write_tbl = [0, 'OK']
+        except Exception as ex:
+            r_write_tbl = [1, str(ex)]
+        return r_write_tbl
+    #-------------------------------------------------------------------
+    def get_table_db_with(self, name_tbl):
+        ''' read one table DB  --------------------------------------'''
+        r_get_table_db = []
+        self.conn = sqlite3.connect(self.path_db)
+        try:
+            with self.conn:
+                self.cur = self.conn.cursor()
+                #self.cur.execute("PRAGMA busy_timeout = 3000")   # 3 s
+                self.cur.execute("SELECT * from " + name_tbl)
+                self.table_db = self.cur.fetchall()    # read table name_tbl
+                r_get_table_db = [0, self.table_db]
+        except Exception as ex:
+            r_get_table_db = [1, name_tbl + str(ex)]
 
-def main(args):
-    return 0
+        return r_get_table_db
+#=======================================================================
+
+#=======================================================================
+def main():
+    # init program config
+    dirr = os.path.abspath(os.curdir)
+    db_path_FUT, name_trm, file_path_DATA, log_path = dirr + '\\DB\\term_pack.sqlite', '', '', ''
+
+    return
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+    sys.exit(main())
